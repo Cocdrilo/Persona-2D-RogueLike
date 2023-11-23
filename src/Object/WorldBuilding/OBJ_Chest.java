f86990cdcdeb217aaaa2a5f39c820d86f0a64a34
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2023-11-23 16:52:00 +0100

    Merge branch 'DanielUpdateBranch'

--- src/Object/WorldBuilding/OBJ_Chest.java
+++ src/Object/WorldBuilding/OBJ_Chest.java
@@ -9,8 +9,6 @@ import main.GamePanel;
  */
 public class OBJ_Chest extends Entity {
 
-    GamePanel gp;
-
     /**
      * Creates a chest object with specific attributes.
      *
@@ -24,7 +22,14 @@ public class OBJ_Chest extends Entity {
         collision = true;
         walkDown1 = setUp("/Objects/Cofre");
         isPickupeable = true;
+        
+    }
 
-
+    public void use(){
+        int goldInChest = (int) (Math.random() * 100);
+        gp.playerSe(4);
+        gp.gameState = gp.dialogueState;
+        gp.ui.currentDialogue = "Chest Opened with this gold : " + goldInChest ;
+        gp.party.Leader.addMoney(goldInChest);
     }
 }
